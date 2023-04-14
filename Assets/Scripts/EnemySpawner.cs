16933223910be16306a762f25da81392d7d284e6
Jack-Evans-student <112406787+Jack-Evans-student@users.noreply.github.com>
2023-04-14 10:03:23 -0400

    part

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] WaveConfigSO currentWave;
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    void SpawnEnemies()
+    {
+        Instantiate(currentWave.GetEnemyPrefab(0), 
+                           currentWave.GetStartingWaypoint().position,
+                                                         Quaternion.identity);
+    }
+}
--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -7,16 +7,26 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Wave Congig", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
 
-
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
 
     public Transform GetStartingWaypoint()
     {
         return pathPrefab.GetChild(0);
     }
 
+
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
 
 
     public List<Transform> GetWaypoints()
